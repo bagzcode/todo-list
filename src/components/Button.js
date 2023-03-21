ad88e2e53b3561083c1ecc7778b2d133fcc1d977
bagzcode <bagus.manuaba@gmail.com>
2023-03-21 12:37:41 +0700

    updating Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,21 @@
 import React from "react";
 
-function Button({children, type}){
-    return (
-        <button 
-            class = "button" 
-            //If not submit, then return button
-            type={type === 'submit' ? 'submit' : 'button'}    
-        >
-        {children}
-        </button>
-    )
-    
+function Button({ children, type, onClick }) {
+  return (
+    <button
+      class="button"
+      onClick={onClick}
+      //If not submit, then return button
+      type={type === "submit" ? "submit" : "button"}
+    >
+      {children}
+    </button>
+  );
 }
 
-function FilterButton({children}){
-    return(
-        <select class = "filterbutton">
-            {children}
-        </select>
-    );
+function FilterButton({ children }) {
+  return <select class="filterbutton">{children}</select>;
 }
 
-export {FilterButton};
+export { FilterButton };
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button, { FilterButton } from "./Button";
 import AddTask from "./AddTask";
 
-function Header(){
-    const [modalOpen, setModalOpen] = useState(true);
-    return(
-        <div className = "addfilter">
-            <Button onClick={() => setModalOpen(true)}>
-            +
-            </Button>
-            <FilterButton>
-                <option value = "all">All</option>
-                <option value = "ongoing">On Going</option>
-                <option value = "completed">Completed</option>
-            </FilterButton>
-            <AddTask modalOpen={modalOpen} setModalOpen={setModalOpen}/>
-        </div>
-    )
+function Header() {
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div className="addfilter">
+      <Button onClick={() => setModalOpen(true)}>+</Button>
+      <FilterButton>
+        <option value="all">All</option>
+        <option value="ongoing">On Going</option>
+        <option value="completed">Completed</option>
+      </FilterButton>
+      <AddTask modalOpen={modalOpen} setModalOpen={setModalOpen} />
+    </div>
+  );
 }
 
 export default Header;
